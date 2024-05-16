82cc7826ddd0905da7b36dd15fff0b391e13fadb
joangelm <jemedin7@cougarnet.uh.edu>
2024-05-16 15:14:51 -0500

    Design Home page & add assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./common/Nav";
+import Home from "./pages/Home";
+import "./common/Commoncss.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+import React, { Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        Aos.init({ duration: 2000 });
+    }, []);
+
+    return (
+        <div>
+            <Nav />
+            {/* <Learn /> */}
+            {/* <Home/>
+      <About />
+      <Services/>s
+      <Education/>
+      <Work/>
+      <Contact/> */}
+            <Routes>
+                <Route exact="true" path="/myportfolio" element={<Home />}></Route>
+                <Route path="/myportfolio" element={<Home />}></Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
